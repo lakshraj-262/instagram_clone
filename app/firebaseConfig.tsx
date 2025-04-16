c6d59a4e57eb2c61cd155346c7cd7833a93a9e8e
lakshraj-262 <lakshyarajsingh6880@gmail.com>
2025-04-16 10:36:17 +0530

    Update firebaseConfig.tsx

--- app/firebaseConfig.tsx
+++ app/firebaseConfig.tsx
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA-ZQj4WYQCp0yZr0eQMeIvSyeTEIvXXjA",
-  authDomain: "clone-b36f1.firebaseapp.com",
-  projectId: "clone-b36f1",
-  storageBucket: "clone-b36f1.appspot.com",
-  messagingSenderId: "552737678871",
-  appId: "1:552737678871:web:c885efd4c7bddb7b8ade52",
-  measurementId: "G-NGGZR5N6TQ",
+  apiKey: 
+  authDomain: 
+  projectId: 
+  storageBucket: 
+  messagingSenderId: 
+  appId: 
+  measurementId: 
 };
 
 // Initialize Firebase
